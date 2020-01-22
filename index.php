29a7b32dadfff2792c63af4a27f005e2560e9362
Bam1594 <59908410+Bam1594@users.noreply.github.com>
2020-01-22 14:55:44 +0700

    v.7
    v.7

--- index.php
+++ index.php
@@ -4,17 +4,10 @@
 <h1>smaile in the world</h1>
 
 <?php
-<<<<<<< HEAD
-echo "Hello World! I'm phichaya"<br>;
-echo "Sivuch Tupkwha"<br>;
-echo "Sutthipongz pandee"<br>;
-=======
-echo "Hello World! I'm phichaya";
+echo "Hello World! I'm phichaya"<br/>;
 echo "Sivuch Tupkwha";
 echo "Sutthipongz pandee";
 echo "Ananda";
-
->>>>>>> da633c22b26f475fab517a99fcea8d3df6f6e087
 ?>
 
 </body>
